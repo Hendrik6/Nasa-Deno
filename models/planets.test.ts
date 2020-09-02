0b73f01ca51b34f955c92ba042eeb7f6d77ac0e7
HendrikVE <18201166+Hennots@users.noreply.github.com>
2020-09-02 22:30:53 +0200

    added testcase

--- models/planets.test.ts
+++ models/planets.test.ts
@@ -3,11 +3,57 @@ import {
   assertEquals,
   assertNotEquals,
 } from "https://deno.land/std@0.53.0/testing/asserts.ts";
+import { filtersHabitablePlanets } from "./planets.ts";
 
 //Test runner in the CLI
 //Built in text fixtures with Deno.test().
 //Assertion statements
 
+const HABITABLE_PLANET = {
+  koi_disposition: "CONFIRMED",
+  koi_prad: "1",
+  koi_srad: "1",
+  koi_smass: "1",
+};
+
+const NOT_CONFIRMED = {
+  koi_disposition: "FALSE POSITIVE",
+};
+
+const TOO_LARGE_PLANETARY_RADIUS = {
+  koi_disposition: "CONFIRMED",
+  koi_prad: "1.5",
+  koi_srad: "1",
+  koi_smass: "1",
+};
+
+const TOO_LARGE_SOLAR_RADIUS = {
+  koi_disposition: "CONFIRMED",
+  koi_prad: "1",
+  koi_srad: "1.01",
+  koi_smass: "1",
+};
+
+const TOO_LARGE_SOLAR_MASS = {
+  koi_disposition: "CONFIRMED",
+  koi_prad: "1",
+  koi_srad: "1",
+  koi_smass: "1.04",
+};
+Deno.test("filter only habitable planets", () => {
+  const filtered = filtersHabitablePlanets([
+    HABITABLE_PLANET,
+    NOT_CONFIRMED,
+    TOO_LARGE_PLANETARY_RADIUS,
+    TOO_LARGE_SOLAR_RADIUS,
+    TOO_LARGE_SOLAR_MASS,
+  ]);
+  assertEquals(filtered, [
+    HABITABLE_PLANET,
+  ]);
+});
+
+//Random testing, showing what Deno can do =>
 //Shorter and better
 Deno.test("short example test", () => {
   assertEquals("deno", "deno");
--- models/planets.ts
+++ models/planets.ts
@@ -8,6 +8,19 @@ type Planet = Record<string, string>;
 
 let planets: Array<Planet>;
 
+export function filtersHabitablePlanets(planets: Array<Planet>) {
+  return planets.filter((planet) => {
+    const planetaryRadius = Number(planet["koi_prad"]);
+    const stellarMass = Number(planet["koi_smass"]);
+    const stellarRadius = Number(planet["koi_srad"]);
+
+    return planet["koi_disposition"] === "CONFIRMED" &&
+      planetaryRadius > 0.5 && planetaryRadius < 1.5 &&
+      stellarMass > 0.78 && stellarMass < 1.04 &&
+      stellarRadius > 0.99 && stellarRadius < 1.01;
+  });
+}
+
 async function loadPlanetsData() {
   const path = join("data", "kepler_exoplanets_nasa.csv");
 
@@ -21,16 +34,7 @@ async function loadPlanetsData() {
 
   Deno.close(file.rid);
 
-  const planets = (result as Array<Planet>).filter((planet) => {
-    const planetaryRadius = Number(planet["koi_prad"]);
-    const stellarMass = Number(planet["koi_smass"]);
-    const stellarRadius = Number(planet["koi_srad"]);
-
-    return planet["koi_disposition"] === "CONFIRMED" &&
-      planetaryRadius > 0.5 && planetaryRadius < 1.5 &&
-      stellarMass > 0.78 && stellarMass < 1.04 &&
-      stellarRadius > 0.99 && stellarRadius < 1.01;
-  });
+  const planets = filtersHabitablePlanets(result as Array<Planet>);
 
   return planets.map(
     ((planet) => {
